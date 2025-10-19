e68aa5b1722fea8b9bf68d16143c5ea3b104616c
kelw.prod@gmail.com <kelw.prod@gmail.com>
2025-10-19 15:28:38 -0300

    first prototype of deletableEdge

---
+++ src/components/flow/edges/custom-deletable-edge.tsx
@@ -0,0 +1,61 @@
+import {
+    BezierEdge,
+    EdgeLabelRenderer,
+    type EdgeProps,
+    getBezierPath,
+    useReactFlow,
+} from "@xyflow/react";
+import { X } from "lucide-react";
+
+export default function CustomDeletableEdge(props: EdgeProps) {
+    const {
+        id,
+        sourceX,
+        sourceY,
+        targetX,
+        targetY,
+        sourcePosition,
+        targetPosition,
+    } = props;
+
+    const { setEdges, setNodes, getEdge } = useReactFlow();
+
+    const [_, labelX, labelY] = getBezierPath({
+        sourceX,
+        sourceY,
+        sourcePosition,
+        targetX,
+        targetY,
+        targetPosition,
+    });
+
+    return (
+        <>
+            <BezierEdge {...props} />
+            <EdgeLabelRenderer>
+                <button
+                    type="button"
+                    className="group pointer-events-auto absolute size-5 flex items-center justify-center rounded-full bg-card text-red-400 transition-colors  hover:bg-card-foreground/10"
+                    style={{
+                        transform: `translate(${labelX}px, ${labelY}px) translate(-50%, -50%)`,
+                    }}
+                    onClick={() => {
+                        const edge = getEdge(id);
+                        console.log(edge)
+                        setNodes((nds) =>
+                            nds.map((n) => {
+                                if (n.id === edge?.source) {
+                                    return { ...n, data: { ...n.data, isConnecting: false } };
+                                }
+                                return { ...n, data: { ...n.data } };
+                            })
+                        );
+                        setEdges((edges) => edges.filter((edge) => edge.id !== id))
+                    }}
+                >
+                    <X className="size-3 transition group-hover:scale-75" />
+                </button>
+            </EdgeLabelRenderer>
+        </>
+    );
+}

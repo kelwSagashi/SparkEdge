c03159f9047bcd3422c62ac3956cff915f523e4f
kelw.prod@gmail.com <kelw.prod@gmail.com>
2025-10-28 15:11:35 -0300

    add codeInstance types

--- packages/db/src/services/db.service.d.ts
+++ packages/db/src/services/db.service.d.ts
@@ -5,8 +5,10 @@ export type ServerTypeUpsertValues = typeof Tables.ServerTypesTable.$inferInsert
 export type ServerUpsertValues = typeof Tables.ServersTable.$inferInsert;
 export type ServerEndpointsUpsertValues = typeof Tables.ServerEndpointsTable.$inferInsert;
 export type DeviceUpsertValues = typeof Tables.DeviceTable.$inferInsert;
+export type CodeInstanceUpsertValues = typeof Tables.CodeInstance.$inferInsert;
 
 export type ServerTypeReturningValues = typeof Tables.ServerTypesTable.$inferSelect;
 export type ServerReturningValues = typeof Tables.ServersTable.$inferSelect;
 export type DeviceReturningValues = typeof Tables.DeviceTable.$inferSelect;
 export type ServerEndpointsReturningValues = typeof Tables.ServerEndpointsTable.$inferSelect;
+export type CodeInstanceReturningValues = typeof Tables.CodeInstance.$inferSelect;

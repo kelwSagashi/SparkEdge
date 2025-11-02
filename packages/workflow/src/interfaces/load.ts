97c54fa12cd8b3a0e3f509966e01487cd561dea2
kelw.prod@gmail.com <kelw.prod@gmail.com>
2025-11-02 11:14:57 -0300

    add load types

---
+++ packages/workflow/src/interfaces/load.ts
@@ -0,0 +1,34 @@
+import type { INodeType, INodeTypeDescription } from './node'
+export type LoadingDetails = {
+	className: string;
+	sourcePath: string;
+};
+
+// export type CredentialLoadingDetails = LoadingDetails & {
+// 	supportedNodes?: string[];
+// 	extends?: string[];
+// };
+
+export type NodeLoadingDetails = LoadingDetails;
+
+export type KnownNodesAndCredentials = {
+	nodes: Record<string, NodeLoadingDetails>;
+	// credentials: Record<string, CredentialLoadingDetails>;
+};
+
+export interface LoadedNodesAndCredentials {
+	nodes: INodeTypeData;
+}
+
+export interface LoadedClass<T> {
+	sourcePath: string;
+	type: T;
+}
+type LoadedData<T> = Map<string, LoadedClass<T>>;
+
+export type INodeTypeData = LoadedData<INodeType>;
+
+export type Types = {
+	nodes: INodeTypeDescription[];
+	// credentials: ICredentialType[];
+};

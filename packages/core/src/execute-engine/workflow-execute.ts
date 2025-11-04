3158862c79c03516cc4ae95feb0440cb941c2ce3
kelw.prod@gmail.com <kelw.prod@gmail.com>
2025-11-03 23:58:21 -0300

    starting execute logic

--- packages/core/src/execute-engine/workflow-execute.ts
+++ packages/core/src/execute-engine/workflow-execute.ts
@@ -1,18 +1,45 @@
-import { ExecutionStatus, INodeData } from "nmg8-workflow";
+import { ExecutionStatus, INode, INodeData, Workflow } from "nmg8-workflow";
 
-interface Workflow {};
-
+type WorkflowModeExecution = 'selected' | 'all';
 export class WorkflowExecute {
-    private status: ExecutionStatus = 'new';
+    private status: ExecutionStatus;
     private readonly abortController = new AbortController();
 	timedOut: boolean = false;
+    mode: WorkflowModeExecution;
 
-    run(
+    private workflow: Workflow;
+
+    
+
+    constructor( 
         workflow: Workflow,
-        startNode?: INodeData,
+        mode: WorkflowModeExecution,
+        startNode?: INodeData
     ) {
+        this.mode = mode;
+        this.status = 'new';
+
+        this.workflow = workflow;
+
+        if (mode === 'selected') {
+            this.workflow = workflow.getOnlySelected();
+        }
+    }
+
+    run() {
         this.status = 'running';
 
+        
+        for (const nodeId in this.workflow.buildGraph()) {
+            const node = this.workflow.nodes.find(n => n.id === nodeId);
+            
+            if (!node) continue;
+
+            this.executeNode(node);
+        }
+    }
+
+    executeNode(node: INode) {
 
     }
 }

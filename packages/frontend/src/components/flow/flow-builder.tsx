5327246da21e07b9a68ca4b834f1fe787d45e298
kelw.prod@gmail.com <kelw.prod@gmail.com>
2025-11-17 16:20:40 -0300

    remove use effect

--- packages/frontend/src/components/flow/flow-builder.tsx
+++ packages/frontend/src/components/flow/flow-builder.tsx
@@ -5,11 +5,6 @@ import { useParams } from "react-router-dom";
 
 export const FlowBuilderPage = () => {
     const { id } = useParams<{ id: string }>();
-    //   const setWorkflow = useFlowStore((s) => s.actions.setWorkflow)
-
-    //   useEffect(() => {
-    //     setWorkflow(workflow)
-    //   }, [setWorkflow, workflow])
 
     return <ReactFlowProvider>
         <div className="absolute inset-0 flex h-full w-full">

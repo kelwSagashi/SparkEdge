c2e5e39689c6cc45f0be1d29f4b99e348e665321
kelw.prod@gmail.com <kelw.prod@gmail.com>
2025-11-17 16:19:36 -0300

    add store and list nodes by api

--- packages/frontend/src/components/flow/node-command.tsx
+++ packages/frontend/src/components/flow/node-command.tsx
@@ -1,24 +1,41 @@
 import React from "react";
 import { CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "../ui/command";
+import { api } from "@/server/server.service";
+import type { INodeTypeDescription } from "nmg8-workflow";
+import { useAddNodeOnEdgeDropStore } from "@/stores/add-node-on-edge-drop-store";
+import { useShallow } from "zustand/react/shallow";
 
 export function NodeCommandDialog({
-    open,
-    setOpen,
     addNode
 }: {
-    open: boolean,
-    setOpen: React.Dispatch<React.SetStateAction<boolean>>
-    addNode: () => void
+    addNode: (name: string) => Promise<void>
 }) {
+    const [
+        open,
+        setOpen
+    ] = useAddNodeOnEdgeDropStore(
+        useShallow((s) => [
+            s.showMenu,
+            s.actions.setShowMenu
+        ])
+    );
+
     const [search, setSearch] = React.useState('');
+    const [nodes, setNodes] = React.useState<Partial<INodeTypeDescription>[]>([]);
+
+    React.useEffect(() => {
+        const getNodes = async () => {
+            const response = await api.getNodes();
+            setNodes(response.data.nodes);
+        }
+        getNodes();
+    }, []);
 
     React.useEffect(() => {
         const down = (e: KeyboardEvent) => {
-            if (e.key === " "
-                // && (e.metaKey || e.ctrlKey)
-            ) {
+            if (e.key === " ") {
                 e.preventDefault()
-                setOpen((open) => !open)
+                setOpen(true)
             }
         }
 
@@ -33,11 +50,19 @@ export function NodeCommandDialog({
                 <CommandList className='border-none text-primary p-2'>
                     <CommandEmpty>Nenhum resultado encontrado!</CommandEmpty>
                     <CommandGroup className="" heading="Nós">
-                        <button className="w-full" onClick={() => { addNode(); setOpen(false); }}>
-                            <CommandItem className='text-primary'>
-                                <span>Script</span>
-                            </CommandItem>
-                        </button>
+                        {nodes.map(node => (
+                            <div key={node.name}>
+                                <button className="w-full" onClick={() => { 
+                                    if (node.name) addNode(node.name); 
+                                    setOpen(false); 
+                                }}>
+                                    <CommandItem className='text-primary'>
+                                        <span>{node.name}</span>
+                                        <span>{node.description}</span>
+                                    </CommandItem>
+                                </button>
+                            </div>
+                        ))}
                     </CommandGroup>
                 </CommandList>
             </CommandDialog>

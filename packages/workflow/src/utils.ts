e9b5ad43c623c074556e5d22ade602ac4bb9237c
kelw.prod@gmail.com <kelw.prod@gmail.com>
2025-11-17 14:45:11 -0300

    feat add deepCopy to utils

--- packages/workflow/src/utils.ts
+++ packages/workflow/src/utils.ts
@@ -16,3 +16,43 @@ export function randomInt(min: number, max?: number): number {
 	}
 	return min + (crypto.getRandomValues(new Uint32Array(1))[0] % (max - min));
 }
+
+export type Primitives = string | number | boolean | bigint | symbol | null | undefined;
+
+export const deepCopy = <T extends ((object | Date) & { toJSON?: () => string }) | Primitives>(
+	source: T,
+	hash = new WeakMap(),
+	path = '',
+): T => {
+	const hasOwnProp = Object.prototype.hasOwnProperty.bind(source);
+	// Primitives & Null & Function
+	if (typeof source !== 'object' || source === null || typeof source === 'function') {
+		return source;
+	}
+	// Date and other objects with toJSON method
+	// TODO: remove this when other code parts not expecting objects with `.toJSON` method called and add back checking for Date and cloning it properly
+	if (typeof source.toJSON === 'function') {
+		return source.toJSON() as T;
+	}
+	if (hash.has(source)) {
+		return hash.get(source);
+	}
+	// Array
+	if (Array.isArray(source)) {
+		const clone = [];
+		const len = source.length;
+		for (let i = 0; i < len; i++) {
+			clone[i] = deepCopy(source[i], hash, path + `[${i}]`);
+		}
+		return clone as T;
+	}
+	// Object
+	const clone = Object.create(Object.getPrototypeOf({}));
+	hash.set(source, clone);
+	for (const i in source) {
+		if (hasOwnProp(i)) {
+			clone[i] = deepCopy((source as any)[i], hash, path + `.${i}`);
+		}
+	}
+	return clone;
+};

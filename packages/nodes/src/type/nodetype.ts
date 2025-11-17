977358c7c7877008d9811f54263d720902463c58
kelw.prod@gmail.com <kelw.prod@gmail.com>
2025-11-17 14:48:00 -0300

    add NodeType abstract class

---
+++ packages/nodes/src/type/nodetype.ts
@@ -0,0 +1,41 @@
+import { INodeTypeExecutionContext, INodeInputConfiguration, INodeOutputConfiguration, INodeType, INodeTypeDescription, INodeTypeReturns, NodeOutput, IExecuteFunctions } from "nmg8-workflow";
+
+export abstract class NodeType implements INodeType {
+	abstract description: INodeTypeDescription;
+
+	getInputs(context: INodeTypeExecutionContext): Promise<INodeTypeReturns<Array<INodeInputConfiguration>>> {
+		throw Error("NotImpelemented");	
+	}
+
+	getOutputs(context: INodeTypeExecutionContext): Promise<INodeTypeReturns<Array<INodeOutputConfiguration>>> {
+		throw Error("NotImpelemented");	
+	}
+
+	getProperties(): INodeTypeDescription {
+		throw Error("NotImpelemented");	
+	}
+
+	test?(context: INodeTypeExecutionContext): Promise<INodeTypeReturns<any>>;
+
+
+	execute?(context: IExecuteFunctions): Promise<NodeOutput>;
+
+	callMethod(
+		method: keyof INodeType,
+		context: INodeTypeExecutionContext
+	) {
+		const instance: INodeType = this;
+		switch (method) {
+			case 'getInputs':
+				return instance.getInputs(context);
+			case 'getOutputs':
+				return instance.getOutputs(context);
+			default:
+				throw new Error(`Método ${String(method)} não existe`);
+		}
+		// const fn = instance[method];
+		// if (typeof fn === "function") {
+		// 	return Promise.resolve((fn as any).apply(this, context));
+		// }
+	}
+}

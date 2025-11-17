d417ceb0a55b99cf0a95c6a2e6e0b9f55c6d7c8e
kelw.prod@gmail.com <kelw.prod@gmail.com>
2025-11-17 15:10:06 -0300

    add workflow service

---
+++ packages/cli/src/workflows/workflow.service.ts
@@ -0,0 +1,14 @@
+import { WorkflowRunner } from "@/workflow-runner";
+import { Service } from "@nmg8/di";
+import { IWorkflowBase } from "nmg8-workflow";
+
+@Service()
+export class WorkflowService {
+    constructor(
+        private readonly workflowRunner: WorkflowRunner,
+    ) {}
+
+    async runTest(workflow: IWorkflowBase, destinationNode?: string) {
+        return this.workflowRunner.runTest(workflow, destinationNode)
+    }
+}

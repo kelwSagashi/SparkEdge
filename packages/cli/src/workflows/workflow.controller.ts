3c3814b2269639b8b8d29dd1dc2cbfe7e79fd644
kelw.prod@gmail.com <kelw.prod@gmail.com>
2025-11-17 15:10:42 -0300

    add workflow controller and fix types

--- packages/cli/src/workflows/workflow.controller.ts
+++ packages/cli/src/workflows/workflow.controller.ts
@@ -1,5 +1,48 @@
-export class WorkflowController {
-    async runManualy() {
+import { Body, Post, RestController } from "@nmg8/di";
+import { WorkflowService } from "./workflow.service";
+import { INode, IWorkflowBase } from "nmg8-workflow";
+import { WorkflowRequest } from "./workflow.request";
+import { WorkflowEntity } from 'nmg8-db/src/types';
+import { dbManager } from 'nmg8-db';
+import { Logger } from "@/simple-logger";
+import { Response } from "express";
 
+@RestController('/workflows')
+export class WorkflowController {
+    constructor(
+        private readonly workflowService: WorkflowService,
+        private readonly logger: Logger
+    ) {}
+
+    @Post('/')
+    async create(req: WorkflowRequest.Create) {
+        // const newWorkflow = new WorkflowEntity({
+        //     name: req.body.name,
+        //     active: req.body.active,
+        //     edges: req.body.edges,
+        //     nodes: req.body.nodes,
+        //     id: req.body.id,
+        //     isArchived: req.body.isAchived,
+        //     settings: req.body.settings ?? {}
+        // });
+
+        // const savedWorkflow = dbManager.upsertWorkflow(newWorkflow);
+
+        // if (!savedWorkflow) {
+		// 	this.logger.log('Failed to create workflow');
+		// 	throw new Error('Failed to save workflow');
+		// }
+    }
+
+    @Post('/run/test')
+    async runTest(
+        req: WorkflowRequest.ManualRun,
+        res: Response
+    ) {
+        console.log('run test', req.body.workflow)
+        return await this.workflowService.runTest(
+            req.body.workflow,
+            req.body.destinationNode
+        );
     }
 }
--- packages/cli/src/workflows/workflow.request.ts
+++ packages/cli/src/workflows/workflow.request.ts
@@ -1,3 +1,22 @@
+import { INode, IEdge, IWorkflowSettings, IWorkflowBase, AuthenticatedRequest } from 'nmg8-workflow';
+
 export declare namespace WorkflowRequest {
-    
+    type CreateUpdatePayload = {
+		id?: string;
+		name: string;
+		nodes: INode[];
+		edges: IEdge[];
+		settings?: IWorkflowSettings;
+		active: boolean;
+		isAchived?: boolean;
+	};
+
+    type RunPayload = {
+        workflow: IWorkflowBase;
+        destinationNode?: string;
+    }
+
+    type Create = AuthenticatedRequest<{}, {}, CreateUpdatePayload>;
+
+    type ManualRun = AuthenticatedRequest<{}, {}, RunPayload, {}>;
 }

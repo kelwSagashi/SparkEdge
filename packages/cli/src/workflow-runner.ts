f539ca6753ec60683859fd44b42dda62ada10ac1
kelw.prod@gmail.com <kelw.prod@gmail.com>
2025-11-20 20:52:55 -0300

    remove run method and inject correct dependencies to workflow

--- packages/cli/src/workflow-runner.ts
+++ packages/cli/src/workflow-runner.ts
@@ -1,6 +1,6 @@
 import { Service } from "@nmg8/di";
 import { NodeTypes } from "./node-types";
-import { IWorkflowBase, IWorkflowExecutionDataProcess, Workflow, WorkflowTest } from "nmg8-workflow";
+import { IWorkflowBase, Workflow } from "nmg8-workflow";
 import { WorkflowExecute } from "nmg8-core";
 
 @Service()
@@ -9,17 +9,11 @@ export class WorkflowRunner {
         private readonly nodeTypes: NodeTypes,
     ) {}
 
-    run(
-        data: IWorkflowExecutionDataProcess
-    ) {
-        const {} = data.workflowData;
-    }
-
     async runTest(workflow: IWorkflowBase, destinationNode?: string) {
         try { 
-            const currentWorkflow = new Workflow(workflow);
-            const workflowExecute = new WorkflowExecute('manual');
-            return workflowExecute.run(currentWorkflow, destinationNode);
+            const currentWorkflow = new Workflow({...workflow, nodeTypes: this.nodeTypes});
+            const workflowExecute = new WorkflowExecute('manual', currentWorkflow);
+            return await workflowExecute.run(destinationNode);
         } catch (error) {
             console.log('workflow-runner', error)
         }

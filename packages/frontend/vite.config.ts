37e60f65704df9b1562f332b8cc0fa0733fb6d16
kelw.prod@gmail.com <kelw.prod@gmail.com>
2025-11-23 15:07:02 -0300

    fix nodemon hot-reloading and server compilation

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -19,6 +19,14 @@ export default defineConfig({
     exclude: ["react-resizable-panels"],
   },
   server: {
+    watch: {
+      ignored: [
+        '!**/node_modules/nmg8-workflow/**',
+        '!**/node_modules/nmg8-db/**',
+        '!**/node_modules/nmg8-nodes/**',
+        '!**/node_modules/nmg8-core/**',
+      ]
+    },
     proxy: {
       '/api': {
         target: 'http://localhost:3000',

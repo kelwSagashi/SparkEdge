a0e23d166cebdaa226bf770f1082baaed0dce67f
kelw.prod@gmail.com <kelw.prod@gmail.com>
2026-01-04 18:53:04 -0300

    add component to show panel data

--- packages/frontend/src/components/flow/nodes/panel.tsx
+++ packages/frontend/src/components/flow/nodes/panel.tsx
@@ -6,32 +6,22 @@ import { ArrowLeft } from "lucide-react";
 import React, { memo, useCallback, useEffect, useState } from "react";
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import NodeOptionSelector from "./properties/options-selector";
+import NodeOptionsProperty from "./properties/options-selector";
 import { api } from "@/server/server.service";
 import type { INodeProperties } from "nmg8-workflow";
 import type { INode } from "@/interfaces";
 import type { INodeExecutionData, INodeTypeDescription } from "nmg8-workflow";
 import { useWorkflowStore } from "@/stores/workflow-store";
 import { useShallow } from "zustand/react/shallow";
-import { ScrollArea } from "@/components/ui/scroll-area";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { JsonViewMain } from "@/components/json-view/json-view";
+import { NodeProperty } from "./properties/properties";
 
 interface INodePanelProps {
     isDialogOpen: boolean;
     setIsDialogOpen: React.Dispatch<React.SetStateAction<boolean>>;
     onClose: () => void;
 }
-
-const RenderParam = memo(({ property }: { property: INodeProperties }) => {
-    switch (property.type) {
-        case "options":
-            // return <NodeOptionSelector />
-            
-        default:
-            return null;
-    }
-});
-
 
 export const NodePanel: React.FC<INodePanelProps> = React.memo(({
     isDialogOpen,
@@ -51,7 +41,7 @@ export const NodePanel: React.FC<INodePanelProps> = React.memo(({
     ]))
     const [description, setDescription] = useState<INodeTypeDescription>();
     const [nodeRunData, setNodeRunData] = useState<INodeExecutionData>();
-    const [nodeIputRunData, setNodeIputRunData] = useState<INodeExecutionData[]>();
+    const [nodeIputRunData, setNodeIputRunData] = useState<Record<string, INodeExecutionData>>();
 
     useEffect(() => {
         if (nodeClicked?.id) {
@@ -65,36 +55,32 @@ export const NodePanel: React.FC<INodePanelProps> = React.memo(({
             const connectedEdges = edges.filter(edge => edge.target === nodeClicked.id);
             console.log(connectedEdges, lastRun)
             
-            const inputs: INodeExecutionData[]  = [];
+            const inputs: Record<string, INodeExecutionData>  = {};
             for (const node of connectedEdges){
                 const data = getNodeRunDataOutput(node.source);
                 if (!data) continue;
-                inputs.push(data);
+                inputs[node.sourceHandle || node.source] = (data);
             };
             setNodeIputRunData(inputs);
         }
     }, [nodeClicked, edges, lastRun, setNodeIputRunData]);
 
-    const renderParams = useCallback(() => {
-        if (!description) return null;
-
-        return (
-            <>
-                {description.properties.map((property, index) => <RenderParam key={index} property={property}/>)}
-            </>
-        )
-
-    }, [description?.properties]);
 
     const handleLoadDescription = useCallback(async () => {
         if (!nodeClicked) return;
-        const _description = (await api.getNodeDescription({name: nodeClicked.data.parameters.type})).data;
+        const _description = (await api.getNodeDescription({name: nodeClicked.data.name})).data;
         setDescription(_description);
-    }, [nodeClicked?.data.parameters.type]);
+
+        _description.properties.forEach(item => {
+            
+        })
+    }, [nodeClicked]);
 
     useEffect(() => {
         handleLoadDescription();
     }, [handleLoadDescription])
+
+    useEffect(() => {console.log('panel', description)}, [description])
 
     return (
 
@@ -108,17 +94,26 @@ export const NodePanel: React.FC<INodePanelProps> = React.memo(({
                 </DialogClose>
                 <PanelGroup className="h-full" direction="horizontal">
                     <Panel defaultSize={30} minSize={20}>
-                        <div className="bg-background w-full h-full mt-6 p-4 rounded-l-2xl">
-                            <h1 className="text-primary">Entrada</h1>
-                            <ScrollArea className='h-full w-full'>
+                        <div className="bg-background w-full h-full mt-6 p-4 rounded-l-2xl flex flex-col">
+                            <div>
+                                <h1 className="text-primary">Entrada</h1>
+                            </div>
+                            <div className="flex-1 overflow-auto mb-6">
+                            <ScrollArea className='h-full w-full flex overflow-auto' >
+                                <div>
                                 <JsonViewMain
+                                    className=""
                                     inputProps={{
-                                        className: "w-auto border-border rounded",
+                                        className: "w-auto border-border rounded ",
                                         disabled: true,
                                     }}
                                     data={nodeIputRunData || {}}
                                 />
+                                </div>
+                                <ScrollBar orientation="vertical"/>
+                                <ScrollBar orientation="horizontal"/>
                             </ScrollArea>
+                            </div>
                         </div>
                     </Panel>
                     <PanelResizeHandle hitAreaMargins={{ coarse: 40, fine: 20 }} className="" />
@@ -132,7 +127,12 @@ export const NodePanel: React.FC<INodePanelProps> = React.memo(({
                                         <TabsTrigger value="b">Configurações</TabsTrigger>
                                     </TabsList>
                                     <TabsContent value="a">
-                                        {renderParams()}
+                                        {description && nodeClicked && (
+                                            <>
+                                                <h1 className={"text-amber-50"}>TETSE</h1>
+                                                <NodeProperty mode="panel" node={nodeClicked} properties={description?.properties} />
+                                            </>
+                                        )}
                                     </TabsContent>
                                     <TabsContent value="b">Configurações</TabsContent>
                                 </Tabs>
@@ -141,18 +141,26 @@ export const NodePanel: React.FC<INodePanelProps> = React.memo(({
                     </Panel>
                     <PanelResizeHandle hitAreaMargins={{ coarse: 40, fine: 20 }} />
                     <Panel defaultSize={30} minSize={20}>
-                        <div className="bg-background w-full h-full mt-6 p-4 rounded-r-2xl">
-                            <h1 className="text-primary">Saída</h1>
-                            <ScrollArea className='h-full w-full'>
-                                <JsonViewMain
-                                    inputProps={{
-                                        className: "w-auto border-border rounded",
-                                        disabled: true
-                                    }}
+                        <div className="bg-background w-full h-full mt-6 p-4 rounded-r-2xl flex flex-col">
+                            <div>
+                                <h1 className="text-primary">Saída</h1>
+                            </div>
+                            <div className="flex-1 overflow-auto mb-6">
+                                <ScrollArea className='h-full w-full flex overflow-auto'>
+                                    <div>
+                                        <JsonViewMain
+                                            inputProps={{
+                                                className: "w-auto border-border rounded",
+                                                disabled: true
+                                            }}
 
-                                    data={nodeRunData || {}}
-                                />
-                            </ScrollArea>
+                                            data={nodeRunData || {}}
+                                        />
+                                    </div>
+                                    <ScrollBar orientation="vertical"/>
+                                    <ScrollBar orientation="horizontal"/>
+                                </ScrollArea>
+                            </div>
                         </div>
                     </Panel>
                 </PanelGroup>

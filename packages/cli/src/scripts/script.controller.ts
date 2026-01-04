d42dd678159e1dbae22f909f286ceb7d5aa200f5
kelw.prod@gmail.com <kelw.prod@gmail.com>
2026-01-04 18:13:53 -0300

    load scripts from repository

--- packages/cli/src/scripts/script.controller.ts
+++ packages/cli/src/scripts/script.controller.ts
@@ -1,76 +1,18 @@
 import { Get, RestController } from "@nmg8/di";
+import { DatabaseService, dbManager } from "nmg8-db";
 
 @RestController('/scripts')
 export class ScriptsController {
+    repository: DatabaseService;
+    constructor() {
+        this.repository = dbManager;
+    }
+
     @Get('/')
     async getScripts() {
+        const scripts = this.repository.listSampleScripts();
         return {
-            data: [
-                {
-                    author: "system",
-                    created_at: Date.now().toString(),
-                    updated_at: Date.now().toString(),
-                    name: "sample class",
-                    description: "",
-                    id: "1",
-                    language: "python",
-                    main_file_name: "sample_class.py",
-                    path: "/extensions/samples/",
-                    source: "system_repo",
-                    entry_fn: null,
-                    repo: null,
-                    url: null,
-                    version: "1"
-                },
-                {
-                    author: "system",
-                    created_at: Date.now().toString(),
-                    updated_at: Date.now().toString(),
-                    name: "sample class with decorators",
-                    description: "",
-                    id: "2",
-                    language: "python",
-                    main_file_name: "sample_class_dec.py",
-                    path: "/extensions/samples/",
-                    source: "system_repo",
-                    entry_fn: null,
-                    repo: null,
-                    url: null,
-                    version: "1"
-                },
-                {
-                    author: "system",
-                    created_at: Date.now().toString(),
-                    updated_at: Date.now().toString(),
-                    name: "sample imp",
-                    description: "",
-                    id: "3",
-                    language: "python",
-                    main_file_name: "sample_imp.py",
-                    path: "/extensions/samples/",
-                    source: "system_repo",
-                    entry_fn: null,
-                    repo: null,
-                    url: null,
-                    version: "1"
-                },
-                {
-                    author: "system",
-                    created_at: Date.now().toString(),
-                    updated_at: Date.now().toString(),
-                    name: "sample imp with decorators",
-                    description: "",
-                    id: "4",
-                    language: "python",
-                    main_file_name: "sample_imp_dec.py",
-                    path: "/extensions/samples",
-                    source: "system_repo",
-                    entry_fn: null,
-                    repo: null,
-                    url: null,
-                    version: "1"
-                },
-            ]
+            data: scripts.data
         }
     }
 }

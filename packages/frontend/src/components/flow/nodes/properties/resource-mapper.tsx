f1f055ce3ca4c10fc91eb669139f8d215be7e1eb
kelw.prod@gmail.com <kelw.prod@gmail.com>
2026-01-04 18:54:17 -0300

    add simple resource mapper

---
+++ packages/frontend/src/components/flow/nodes/properties/resource-mapper.tsx
@@ -0,0 +1,28 @@
+import { Input } from "@/components/ui/input";
+import type { DisplayOptionsReplaceItem, IDataObject, INodeProperties } from "nmg8-workflow";
+import { useCallback, useEffect, useState } from "react";
+
+type NodePropertiesOptionsType = Extract<INodeProperties, { type: 'resourceMapper' }>;
+
+export interface NodeResourceMapperProps {
+    // handleChangeText: (value: IDataObject, property: NodePropertiesOptionsType) => void;
+    property: NodePropertiesOptionsType;
+}
+
+export default function NodeResourceMapperProperty({
+    // handleChangeText,
+    property
+}: NodeResourceMapperProps) {
+    const [text, setText] = useState<string>("");
+
+    return (
+        <div className='w-full'>
+            <span className="text-sm text-muted-foreground">{property.displayName}</span>
+            <Input value={text} onChange={val => {
+                setText(val.target.value);
+                // handleChangeText({[property.name]: val.target.value}, property);
+            }}
+            placeholder={property.placeholder}/>
+        </div>
+    )
+}

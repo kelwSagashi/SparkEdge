a3b0c5c676246e0e6f1a8c99dce49adc71f96e63
kelw.prod@gmail.com <kelw.prod@gmail.com>
2026-01-04 18:17:54 -0300

    moving seed file

---
+++ packages/db/src/types/seed.ts
@@ -0,0 +1,9 @@
+import type { ServerTypeReturningValues } from './types'
+export const DefaultServerTypes: ServerTypeReturningValues[] = [
+    {
+        id: "http",
+        type: "http",
+        name: "Servidor HTTP REST",
+        description: "http, https, rest services are running as a service that u can consult via http request"
+    }
+]

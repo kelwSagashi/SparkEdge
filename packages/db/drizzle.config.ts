0737c41f4728368c0daf4e7bc0e597ead9602e2a
kelw.prod@gmail.com <kelw.prod@gmail.com>
2026-01-05 15:38:23 -0300

    no changes

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -2,7 +2,8 @@ import 'dotenv/config';
 import type { Config } from 'drizzle-kit';
 
 export default {
-  schema: './src/schema/schemas.ts',
+  // schema was previously pointing to ./src/schema/schemas.ts but our schemas live under ./src/entity
+  schema: './src/entity/schemas.ts',
   out: './drizzle',
   dialect: 'sqlite',
   dbCredentials: {

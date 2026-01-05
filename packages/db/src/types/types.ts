17a1ecc22a1e58b7ef00eaeb160af25fb8303ca2
kelw.prod@gmail.com <kelw.prod@gmail.com>
2026-01-05 15:33:32 -0300

    add new types

--- packages/db/src/types/types.ts
+++ packages/db/src/types/types.ts
@@ -1,4 +1,4 @@
-import { Tables } from '../entity';
+import { Tables } from '../db';
 
 export type ServerTypeUpsertValues = typeof Tables.ServerTypesTable.$inferInsert;
 export type ServerUpsertValues = typeof Tables.ServersTable.$inferInsert;
@@ -14,3 +14,20 @@ export type CodeInstanceReturningValues = typeof Tables.CodeInstance.$inferSelec
 
 export type WorkflowUpsertValues = typeof Tables.Workflow.$inferInsert;
 export type WorkflowReturningValues = typeof Tables.Workflow.$inferSelect;
+
+export type UserUpsertValues = typeof Tables.UsersTable.$inferInsert;
+export type UserReturningValues = typeof Tables.UsersTable.$inferSelect;
+
+export type ProjectUpsertValues = typeof Tables.ProjectsTable.$inferInsert;
+export type ProjectReturningValues = typeof Tables.ProjectsTable.$inferSelect;
+
+export type ProjectMemberUpsertValues = typeof Tables.ProjectMembersTable.$inferInsert;
+export type ProjectMemberReturningValues = typeof Tables.ProjectMembersTable.$inferSelect;
+
+export type WorkflowVersionUpsertValues = typeof Tables.WorkflowVersionsTable.$inferInsert;
+export type WorkflowVersionReturningValues = typeof Tables.WorkflowVersionsTable.$inferSelect;
+
+export type ReturningQueries<T> = {
+	error?: unknown;
+	data: T;
+};
